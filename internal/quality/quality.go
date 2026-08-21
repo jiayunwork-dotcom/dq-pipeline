b3e822505037073b3395b6a5fd358720e7f439a4
“jiayun” <jiayun_work@163.com>
2026-08-21 23:28:33 +0800

    fix: Evaluate 质量分数不再被 dropScore 置 0

--- internal/quality/quality.go
+++ internal/quality/quality.go
@@ -114,9 +114,9 @@ func Evaluate(t *parse.Table, rules []Rule) (Report, error) {
 		rep.ByColumn[v.Column]++
 	}
 	if totalChecks == 0 {
-		rep.Score = applyScore(1)
+		rep.Score = 1
 	} else {
-		rep.Score = applyScore(1 - float64(len(viol))/float64(totalChecks))
+		rep.Score = 1 - float64(len(viol))/float64(totalChecks)
 	}
 	return rep, nil
 }
